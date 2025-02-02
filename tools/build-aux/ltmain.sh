fd0ad151664c670ee59fe852f621aef13d564846
pepijn-devries <peppi2@gmail.com>
2025-02-02 14:12:16 +0100

    Make tokens available when using autobrew

--- tools/build-aux/ltmain.sh
+++ tools/build-aux/ltmain.sh
@@ -535,10 +535,10 @@ func_require_term_colors ()
 
       if test 1 = "$USE_ANSI_COLORS"; then
         # Standard ANSI escape sequences
-        tc_reset='[0m'
-        tc_bold='[1m';   tc_standout='[7m'
-        tc_red='[31m';   tc_green='[32m'
-        tc_blue='[34m';  tc_cyan='[36m'
+        tc_reset='[0m'
+        tc_bold='[1m';   tc_standout='[7m'
+        tc_red='[31m';   tc_green='[32m'
+        tc_blue='[34m';  tc_cyan='[36m'
       else
         # Otherwise trust the terminfo database after all.
         test -n "`tput sgr0 2>/dev/null`" && {
@@ -10031,7 +10031,7 @@ EOF
 	    test yes = "$compiler_needs_object" &&
 	    test -z "$libobjs"; then
 	    # extract the archives, so we have objects to list.
-	    # TODO: could optimize this to just extract one archive.
+	    # NOTE: could optimize this to just extract one archive.
 	    whole_archive_flag_spec=
 	  fi
 	  if test -n "$whole_archive_flag_spec"; then
